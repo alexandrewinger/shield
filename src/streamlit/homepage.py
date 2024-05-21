b9e9e45a9be18d028d679ce0cf46f4911cb9724b
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-21 19:05:01 +0200

    Create homepage.py

---
+++ src/streamlit/homepage.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import requests
+
+localhost = "127.0.0.1"
+
+st.title("SHIELD")
+st.write("Identification")
+
+username = st.text_input("Nom d'utilisateur:")
+password = st.text_input("Mot de passe:")
+id = username + ":" + password
+user_payload = {"identification": id}
+
+
+if st.button("S'identifier"):
+    response = requests.get(url=f"http://{localhost}:8000/get_rights",
+                            headers=user_payload)
+    rights = response.json()
+
+    if 'id' not in st.session_state:
+        st.session_state['id'] = user_payload
+
+    if rights == 0:
+        st.switch_page("pages/standard_user_page.py")
+    else:
+        st.switch_page("pages/admin_page.py")
