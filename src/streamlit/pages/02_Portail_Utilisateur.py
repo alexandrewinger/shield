1d04d2e2099aadb94c652ecf3fb61c2ab9a4f264
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-24 16:08:17 +0200

    Create 02_Portail_Utilisateur.py

---
+++ src/streamlit/pages/02_Portail_Utilisateur.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests # noqa F401
+import os
+from pathlib import Path
+import sys
+
+# internal
+root_path = Path(os.path.realpath(__file__)).parents[3]
+sys.path.append(os.path.join(root_path, "src", "streamlit"))
+from streamlit_library import prepare_jsonl, predict, label_prediction # noqa E402
+
+# -------------- Variables declaration: ---------------------------------------
+localhost = "127.0.0.1"
+# user_payload = st.session_state['id']
+header_admin = {"identification": "admin:4dmin"}
+header_user = {"identification": "fdo:c0ps"}
+
+# --------------- Page --------------------------------------------------------
+st.set_page_config(layout="wide")
+st.title("SHIELD")
+st.write("Bienvenue sur le portail SHIELD standard")
+
+# ----- Predict from call:
+# pred = st.container(border=True)
+# if st.button("Effectuer une prédiction à partir d'un appel:"):
+predict.call()
+# st.switch_page("pages/call_predict_page.py")
+
+# ----- Label prediction:
+
+label_prediction.from_call()
