7eb068cb60ca1d78a487f313391508e57b12c76b
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-28 11:37:51 +0200

    feature(streamlit): add graphs

--- src/streamlit/pages/03_Portail Administrateur.py
+++ src/streamlit/pages/03_Portail Administrateur.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 import os
 from pathlib import Path
+# import plotly.figure_factory as ff
+import plotly.express as px
 import sys
 
 # internal
@@ -17,6 +19,7 @@ header_admin = {"identification": "admin:4dmin"}
 header_user = {"identification": "fdo:c0ps"}
 
 # --------------- Page --------------------------------------------------------
+st.set_page_config(layout="wide")
 st.title("SHIELD")
 st.write("Bienvenue sur le portail SHIELD admin")
 tab0, tab1, tab2, tab3 = st.tabs(["Dbug", "Users", "Model demo", "Monitoring"])
@@ -26,7 +29,7 @@ with tab0:
     data = st.container(border=True)
     option = data.selectbox(
         "Quel type d'information souhaitez-vous?",
-        ("f1_score", "preds_call", "preds_labeled",
+        ("f1_scores", "preds_call", "preds_labeled",
          "preds_test", "train", "update_data"))
     log_data = {"name": option}
 
@@ -53,39 +56,57 @@ with tab1:
     # ----- Get all users:
     users.get_all_users()
 
-# --------------- Model management --------------------------------------------
+# --------------- Model demo --------------------------------------------------
 with tab2:
-    st.write("Model demo")
+    viz, ctrl = st.columns(2)
 
+    # ---------- Control panel:
+    ctrl.write("")
+    ctrl.markdown("#### Panneau de commandes")
     # ----- Get f1_score:
-    if st.button("Obtenir le f1_score actuel"):
+    if ctrl.button("Obtenir le f1_score actuel"):
         response = requests.get(
             url=f"http://{localhost}:8000/get_f1_score",
             headers=header_admin)
         f1_score = response.json()
-        st.write("Le f1_score vaut actuellement", f1_score[0], ".")
+        ctrl.write(f1_score[0])
 
     # ----- Predict from test:
-    if st.button("Effectuer une prédiction à partir de l'ensemble de test"):
+    if ctrl.button("Effectuer une prédiction à partir de l'ensemble de test"):
         response = requests.get(
             url=f"http://{localhost}:8000/predict_from_test",
             headers=header_admin)
-        st.write(response.json())
+        ctrl.write(response.json())
 
     # ----- Label preds test:
-    if st.button("Labelliser les prédictions faites à partir de l'ensemble de test"): # noqa E501
+    if ctrl.button("Labelliser les prédictions faites à partir de l'ensemble de test"): # noqa E501
         response = requests.get(
             url=f"http://{localhost}:8000/label_pred_test",
             headers=header_admin)
-        st.write(response.json()[0])
+        ctrl.write(response.json()[0])
 
     # ----- Update f1_score:
-    if st.button("Mettre le f1_score à jour"):
+    if ctrl.button("Mettre le f1_score à jour"):
         response = requests.get(
             url=f"http://{localhost}:8000/update_f1_score",
             headers=header_admin)
-        st.write(response.json())
+        ctrl.write(response.json())
 
+    # ---------- Vizualisation panel:
+    # ----- Display graph f1_scores:
+    log_data = {"name": "f1_scores"}
+    response = requests.post(url=f"http://{localhost}:8000/get_logs",
+                                 json=log_data,
+                                 headers=header_admin)
+    data_jsonl = response.json()
+    df = prepare_jsonl.jsonl_to_df(data_jsonl)
+
+    fig = px.line(df, y="f1_score_macro_average",
+                  title="Evolution du f1_score")
+
+    event = viz.plotly_chart(fig, key="time_stamp", on_select="rerun")
+    viz.markdown("#### Logs f1_scores:")
+    viz.dataframe(df)
 # --------------- Monitoring and updates --------------------------------------
 with tab3:
     st.write("Monitoring")
