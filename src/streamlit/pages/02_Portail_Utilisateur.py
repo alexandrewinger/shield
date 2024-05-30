0cd528bc808642a3649637996e779afbf52f8f7a
Alexandre Winger <65301787+alexandrewinger@users.noreply.github.com>
2024-05-30 11:46:39 +0200

    Update 02_Portail_Utilisateur.py
    switch context

--- src/streamlit/pages/02_Portail_Utilisateur.py
+++ src/streamlit/pages/02_Portail_Utilisateur.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.join(root_path, "src", "streamlit"))
 from streamlit_library import prepare_jsonl, predict, label_prediction # noqa E402
 
 # -------------- Variables declaration: ---------------------------------------
-localhost = "127.0.0.1"
+localhost = "api" if os.environ.get('ENVIRONMENT') == 'docker' else "127.0.0.1" # noqa E501
 # user_payload = st.session_state['id']
 header_admin = {"identification": "admin:4dmin"}
 header_user = {"identification": "fdo:c0ps"}
